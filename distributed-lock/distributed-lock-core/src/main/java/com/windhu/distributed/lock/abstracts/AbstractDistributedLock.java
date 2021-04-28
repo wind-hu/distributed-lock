40ec8537e659f340e9ad9088e2867434678e3d92
wind-hu <18861453337@163.com>
2021-04-28 14:35:27 +0800

    Create AbstractDistributedLock.java

---
+++ distributed-lock/distributed-lock-core/src/main/java/com/windhu/distributed/lock/abstracts/AbstractDistributedLock.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021-2021. All rights reserved. wind-hu (18861453337@163.com)
+ */
+
+package com.windhu.distributed.lock.abstracts;
+
+import com.windhu.distributed.lock.interfaces.IDistributedLock;
+
+/**
+ * <p>分布式锁抽象</p>
+ *
+ * @author : wind-hu
+ **/
+public abstract class AbstractDistributedLock<T> implements IDistributedLock<T> {
+}
