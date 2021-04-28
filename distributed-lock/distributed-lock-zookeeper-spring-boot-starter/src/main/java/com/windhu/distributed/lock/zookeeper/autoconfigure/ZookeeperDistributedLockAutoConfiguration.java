853e400d7c9f8081fe51b3c623cf4d79d9bb4d09
wind-hu <18861453337@163.com>
2021-04-28 14:57:32 +0800

    Create ZookeeperDistributedLockAutoConfiguration.java

---
+++ distributed-lock/distributed-lock-zookeeper-spring-boot-starter/src/main/java/com/windhu/distributed/lock/zookeeper/autoconfigure/ZookeeperDistributedLockAutoConfiguration.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021-2021. All rights reserved. wind-hu (18861453337@163.com)
+ */
+
+package com.windhu.distributed.lock.zookeeper.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>zookeeper自动转配</p>
+ *
+ * @author : wind-hu
+ **/
+@Configuration
+public class ZookeeperDistributedLockAutoConfiguration {
+}
